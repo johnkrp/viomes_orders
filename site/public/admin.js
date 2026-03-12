03aa1e6c31506dfe775e9b19b5380d53b7d98b16
johnkrp <johnkar2002@gmail.com>
2026-03-12 15:47:05 +0200

    Fix ledger panel text encoding

--- site/public/admin.js
+++ site/public/admin.js
@@ -530,7 +530,7 @@ function resetReceivables() {
   if (els.receivablesBody) {
     els.receivablesBody.innerHTML = `
       <tr>
-        <td colspan="6" class="admin-table-empty">Δεν έχει φορτωθεί ακόμη snapshot υπολοίπων.</td>
+        <td colspan="6" class="admin-table-empty">\u0394\u03b5\u03bd \u03ad\u03c7\u03b5\u03b9 \u03c6\u03bf\u03c1\u03c4\u03c9\u03b8\u03b5\u03af \u03b1\u03ba\u03cc\u03bc\u03b7 snapshot \u03c5\u03c0\u03bf\u03bb\u03bf\u03af\u03c0\u03c9\u03bd.</td>
       </tr>
     `;
   }
@@ -806,7 +806,7 @@ function renderReceivables(receivables) {
         .join("")
     : `
         <tr>
-          <td colspan="6" class="admin-table-empty">Δεν υπάρχουν κινήσεις καρτέλας για αυτόν τον πελάτη.</td>
+          <td colspan="6" class="admin-table-empty">\u0394\u03b5\u03bd \u03c5\u03c0\u03ac\u03c1\u03c7\u03bf\u03c5\u03bd \u03ba\u03b9\u03bd\u03ae\u03c3\u03b5\u03b9\u03c2 \u03ba\u03b1\u03c1\u03c4\u03ad\u03bb\u03b1\u03c2 \u03b3\u03b9\u03b1 \u03b1\u03c5\u03c4\u03cc\u03bd \u03c4\u03bf\u03bd \u03c0\u03b5\u03bb\u03ac\u03c4\u03b7.</td>
         </tr>
       `;
 }
@@ -1282,4 +1282,5 @@ refreshSession({ silent: false }).then((me) => {
     els.username.focus();
   }
 });
+
 
